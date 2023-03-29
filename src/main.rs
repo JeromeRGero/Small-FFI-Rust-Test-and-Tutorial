946b7a044cf40f9e015a86a1c9bac8b10a3fce1b
Jerome Gero <JeromeRGero@gmail.com>
2023-03-29 19:54:51 -0400

    A small example of using FFI in Rust!

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+extern crate libc;
+
+use std::ffi::CString;
+use libc::{strlen, strcmp};
+
+fn main() {
+    let c_string_1 = CString::new("Hello, world!").expect("CString::new failed");
+    let length = unsafe { strlen(c_string_1.as_ptr()) };
+    println!("Length: {}", length);
+    let c_string_2 = CString::new("Hello, world!").expect("CString::new failed");
+    let result: i32 = unsafe { strcmp(c_string_1.as_ptr(), c_string_2.as_ptr()) };
+    println!("Result: {}", result);
+}
