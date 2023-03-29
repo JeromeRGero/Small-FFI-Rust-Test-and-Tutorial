5cde715210fcaa6467e6244391b39afced8ec392
Jerome Gero <JeromeRGero@gmail.com>
2023-03-29 19:58:53 -0400

    Removing icon.png, Adding Comment.

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,6 @@ fn main() {
     println!("Length: {}", length);
     let c_string_2 = CString::new("Hello, world!").expect("CString::new failed");
     let result: i32 = unsafe { strcmp(c_string_1.as_ptr(), c_string_2.as_ptr()) };
+    // returns 0 if equal.
     println!("Result: {}", result);
 }
